97170e3af959dcd3f0aa6188841b1e716db06b6b
dinmukhammed <arystambekdimash@gmail.com>
2023-07-08 15:17:27 +0600

    added fastapi

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-
+from .repository import CommentRepository
 app = FastAPI()
+
+repository = CommentRepository()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
@@ -16,10 +18,28 @@ def home(request:Request):
     return templates.TemplateResponse("main/categories.html", {"request": request})
 
 @app.get("/feeds")
-def home(request:Request,):
-    return templates.TemplateResponse("main/feeds.html", {"request": request})
+def home(request:Request,check:str = "",search:str=""):
+    if check:
+        filtered_category = [i for i in repository.get_all() if i["category"] == check.lower()]
+        filtered_category_children = []
+        for i in filtered_category:
+            if search.lower() in i["comment"].lower():
+                filtered_category_children.append(i)
+        return templates.TemplateResponse("main/feeds.html", {"request": request,"category":filtered_category_children})
+    else:
+        filtered_category = repository.get_all()
+        filtered_category_children = []
+        for i in filtered_category:
+            if search.lower() in i["comment"].lower():
+                filtered_category_children.append(i)
+        return templates.TemplateResponse("main/feeds.html", {"request": request,"category":filtered_category_children})
+
 
 
 @app.get("/form")
+def home(request:Request,):
+    return templates.TemplateResponse("main/form.html", {"request": request})
+
+@app.post("/form")
 def home(request:Request,):
     return templates.TemplateResponse("main/form.html", {"request": request})
--- app/repository.py
+++ app/repository.py
@@ -1,11 +1,12 @@
 class CommentRepository:
-    def __int__(self):
+    def __init__(self):
         self.comments = [
-            {"id" : 1,"user" : 'starboy','comment':'Hello my name is starboy. In this comment i want say about Goverment','category':'positive'},
-            {"id": 2, "user": 'starboy11','comment': 'Hello my name is starboy. In this comment i want say about Goverment', 'category': 'negative'},
-            {"id": 3, "user": 'starboy221','comment': 'Hello my name is starboy. In this comment i want say about Goverment', 'category': 'negative'},
-            {"id": 4, "user": 'starboy33','comment': 'Hello my name is starboy. In this comment i want say about Goverment', 'category': 'positive'},
+            {"id": 1, "user": 'starboy', 'comment': 'Hello11 my name is starboy. In this comment i want say about Government', 'category': 'positive'},
+            {"id": 2, "user": 'starboy11', 'comment': 'Hello my name is starboy. In this comment i want say about Government', 'category': 'negative'},
+            {"id": 3, "user": 'starboy221', 'comment': 'Hello my name is starboy. In this comment i want say about Government', 'category': 'negative'},
+            {"id": 4, "user": 'starboy33', 'comment': 'Hello my name is starboy. In this comment i want say about Government', 'category': 'positive'},
         ]
+
     def get_all(self):
         return self.comments
 
